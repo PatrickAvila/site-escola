59325c99686acbd31c4515b52f9903df2a4cae1a
Patrick Ávila Ferreira da Costa <patrick.costa@vericode.com.br>
2025-09-26 16:59:32 -0300

    Commit inclusão nova funcionalidade Contato

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Altere aqui para alternar entre local e produção
-const API_URL = 'https://escola-backend-45kv.onrender.com';
-//const API_URL = 'http://localhost:3000'; // use esta linha para desenvolvimento local
+//const API_URL = 'https://escola-backend-45kv.onrender.com';
+const API_URL = 'http://localhost:3000'; // use esta linha para desenvolvimento local
